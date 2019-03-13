261acb617ffd213a5bfb032087bbb1cb427c5eae
Iacomussi <Iacomussi@172.21.14.20>
2019-03-13 13:35:42 +0100

    Lab 2 versione 1 con mappa

--- Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -1,13 +1,11 @@
 package it.polito.tdp.alien;
 
-/**
- * Sample Skeleton for 'Alien.fxml' Controller Class
- */
-
-
-
 import java.net.URL;
+import java.security.InvalidParameterException;
 import java.util.ResourceBundle;
+import java.util.*;
+
+import com.sun.javafx.collections.MappingChange.Map;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,39 +15,72 @@ import javafx.scene.control.TextField;
 
 public class AlienController {
 	
+	private HashMap <String, String> parole =  new HashMap<String,String>();
+
     @FXML
     private ResourceBundle resources;
+
     @FXML
     private URL location;
+
     @FXML
     private TextField txtWord;
-    @FXML
-    private TextArea txtResult;
+
     @FXML
     private Button btnTranslate;
+
+    @FXML
+    private TextArea txtMessaggi;
+
     @FXML
     private Button btnReset;
-        
-    
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-    	assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
-    	
-    }
-  
-    
-    @FXML
-    void doTranslate(ActionEvent event) {
-    	    	
-    }
-    
-    
+
     @FXML
     void doReset(ActionEvent event) {
 
     }
-    
+
+    @FXML
+    void doTranslate(ActionEvent event) {
+
+    	//this.txtMessaggi.clear();
+    	String parola = this.txtWord.getText();
+    	String traduzione;
+    	String [] parts = parola.trim().split(" ");
+    	
+    	  	
+    	if(parts.length==2) { //memorizzo la parola
+    		
+    		if(!parts[0].matches("[a-zA-Z]+") || !parts[1].matches("[a-zA-Z]+")) {
+    			txtMessaggi.appendText("Devi inserire una parola!\n");
+    			throw new InvalidParameterException("Devi inserire una parola!\n");
+    		}
+    		parole.put(parts[0].toLowerCase(), parts[1].toLowerCase());
+    		txtMessaggi.appendText("Inserita nuova parola al dizionario\n");
+    	}
+  
+    	else if(parts.length==1) {
+    		
+    		if(!parts[0].matches("[a-zA-Z]+")) {
+    			txtMessaggi.appendText("Devi inserire una parola!\n");
+    			throw new InvalidParameterException("Devi inserire una parola!\n");
+    		}
+    		traduzione = parole.get(parola.toLowerCase());
+    		if(traduzione == null) {
+    			txtMessaggi.appendText("Parola non presente.\n");
+    		} else {
+    			txtMessaggi.appendText(parola+ " significa "+traduzione+"\n");
+    		}
+    	}
+    	txtWord.clear();
+    }
+
+    @FXML
+    void initialize() {
+        assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert txtMessaggi != null : "fx:id=\"txtMessaggi\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
+
+    }
 }
